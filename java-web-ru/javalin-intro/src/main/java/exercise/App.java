08fec7c3b60b07316e7f4c394140509139a94d18
Artur Fayzullin <fayzullin.ar@mail.ru>
2025-05-03 16:04:24 +0300

    fsdf

--- java-web-ru/javalin-intro/src/main/java/exercise/App.java
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -1,11 +1,14 @@
 package exercise;
 
+// BEGIN
 import io.javalin.Javalin;
+// END
 
 public final class App {
 
     public static Javalin getApp() {
 
+        // BEGIN
         Javalin app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
@@ -15,6 +18,7 @@ public final class App {
         });
 
         return app;
+        // END
     }
 
     public static void main(String[] args) {
