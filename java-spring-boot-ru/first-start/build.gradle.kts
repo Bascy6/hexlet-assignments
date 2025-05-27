e6cb52acc873e964e2e22ce5fdc6b781609fa67c
@hexlet/cli <support@hexlet.io>
2025-05-27 19:11:51 +0300

    submit java-spring-boot-ru/first-start

--- java-spring-boot-ru/first-start/build.gradle.kts
+++ java-spring-boot-ru/first-start/build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 }
 
 dependencies {
+
     // BEGIN
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
