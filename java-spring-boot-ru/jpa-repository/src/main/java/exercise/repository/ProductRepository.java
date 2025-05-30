735f00dca565192f575f06a0dd3fa25ea85c0464
Artur Fayzullin <fayzullin.ar@mail.ru>
2025-05-30 18:49:32 +0300

    add

---
+++ java-spring-boot-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import exercise.model.Product;
+
+import org.springframework.data.domain.Sort;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // BEGIN
+    public List<Product> findAllByPriceGreaterThan(int min);
+    public List<Product> findAllByPriceLessThan(int max);
+    public List<Product> findAllByPriceBetweenOrderByPriceAsc(int min, int max);
+    // END
+}
